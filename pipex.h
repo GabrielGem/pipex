df7b2da78a3fb494e9f2025b5376cb27c41b5c88
gabrgarc <gabrgarc@student.42sp.org.br>
2025-12-14 20:29:42 -0300

    add: stuct and functions prototypes

--- pipex.h
+++ pipex.h
@@ -6,7 +6,7 @@
 /*   By: gabr <gabrgarc@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/08 11:34:32 by gabr              #+#    #+#             */
-/*   Updated: 2025/12/08 12:20:49 by gabr             ###   ########.fr       */
+/*   Updated: 2025/12/14 20:19:42 by gabrgarc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,5 +21,36 @@
 # include <sys/wait.h>// wait waitpid
 # include "ft_printf.h"
 # include "libft.h"
+
+typedef struct s_info
+{
+	char	**cmd1;
+	char	*path_cmd1;
+	char	**cmd2;
+	char	*path_cmd2;
+	char	**path;
+	char	**envp;
+	char	*file1;
+	char	*file2;
+	char	flags;
+}	t_info;
+
+enum e_files
+{
+	infile,
+	outfile
+};
+
+enum e_relative
+{
+	relative = 0b1,
+	relative2 = 0b10
+};
+
+t_info	get_info(char **argv, char **envp);
+
+void	pipex(t_info *info);
+void	input_validations(t_info *info);
+void	clear_memory(t_info *info);
 
 #endif
