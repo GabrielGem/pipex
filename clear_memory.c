2123516117fc04adc4d42174229c6ceb911c33c3
gabrgarc <gabrgarc@student.42sp.org.br>
2025-12-14 20:30:56 -0300

    function for clear struct

---
+++ clear_memory.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   clear_memory.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gabrgarc <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/11 15:44:37 by gabrgarc          #+#    #+#             */
+/*   Updated: 2025/12/14 17:00:34 by gabrgarc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pipex.h"
+
+void	clear_memory(t_info *info)
+{
+	int	i;
+
+	i = -1;
+	while (info->path[++i])
+		free(info->path[i]);
+	i = -1;
+	while (info->cmd1[++i])
+		free(info->cmd1[i]);
+	if (info->flags & relative)
+		free(info->path_cmd1);
+	i = -1;
+	while (info->cmd2[++i])
+		free(info->cmd2[i]);
+	if (info->flags & relative2)
+		free(info->path_cmd2);
+	free(info->path);
+}
