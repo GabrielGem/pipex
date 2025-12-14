4fec28c4cdd2fe28ff6c1e05776e6f0122faff8a
gabrgarc <gabrgarc@student.42sp.org.br>
2025-12-14 20:28:53 -0300

    separate logic for other functions

--- main.c
+++ main.c
@@ -6,86 +6,24 @@
 /*   By: gabrgarc <gabrgarc@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/06 12:39:54 by gabrgarc          #+#    #+#             */
-/*   Updated: 2025/12/09 18:03:57 by gabrgarc         ###   ########.fr       */
+/*   Updated: 2025/12/14 14:08:21 by gabrgarc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-int	main(int argc, char **argv)
+int	main(int argc, char **argv, char **envp)
 {
-	input_validations(argv);
-}
+	t_info	info;
 
-void	input_validations(argv);
-{
-	char	**paths;
-
-	paths = get_env();
-	handle_command(argv[2], paths);
-	handle_command(argv[3], paths);
-	handle_file(char *file); TODO
-}
-
-char	**get_env(void)
-{
-	int		fd;
-	char	*path;
-	char	*env;
-	char	*paths;
-	int		i;
-	
-	fd = open("/etc/environment", O_RDONLY);
-	path = get_next_line(fd);
-	env = ft_strtrim(path, "PATH=\"\n");
-	free(path);
-	paths = ft_split(env, ':');
-	free(env);
-	i = 0;
-	while (paths[i])
-	{
-		env = ft_strjoin(paths[i], "/");
-		free(paths[i]);
-		paths[i] = env;
-		free(env);
-		i++;
-	}
-	return (paths);
-}
-
-void	handle_command(char *cmd, char **paths)
-{
-	char	*absolute;
-
-	while (*paths)
-	{
-		absolute = ft_strjoin(paths, cmd);
-		if (access(absolute, F_OK) == 0)
-			//''
-		if (access(absolute, X_OK) == 0)
-			//passed to a struct?
-		paths++;
-	}
-	close(fd);
-	return (NULL);
-}
-
-void	handle_file(int argc, char **argv)
-{
 	if (argc < 5 || argc > 5)
 	{
 		ft_printf("Error: Invalid number of arguments\n");
-		exit(1);
+		return (1);
 	}
-	// file access validation
-	if (access(argv[1], F_OK) != 0) // file exist?
-	{
-		ft_printf("Error: File does not exist\n");
-		exit(1);
-	}
-	if (access(argv[1], R_OK) != 0)
-	{
-		ft_printf("Error: Read permission denied\n");
-		exit(1);
-	}
+	info = get_info(argv, envp);
+	input_validations(&info);
+	pipex(&info);
+	clear_memory(&info);
+	return (0);
 }
