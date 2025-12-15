049776514901d50c24feca80ca69fa31ea260b8a
gabrgarc <gabrgarc@student.42sp.org.br>
2025-12-15 17:04:07 -0300

    Changes for better readability.

--- clear_memory.c
+++ clear_memory.c
@@ -6,7 +6,7 @@
 /*   By: gabrgarc <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/11 15:44:37 by gabrgarc          #+#    #+#             */
-/*   Updated: 2025/12/14 21:13:27 by gabrgarc         ###   ########.fr       */
+/*   Updated: 2025/12/15 10:44:08 by gabrgarc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,20 +16,20 @@ void	clear_memory(t_info *info)
 {
 	int	i;
 
-	i = -1;
-	while (info->path[++i])
-		free(info->path[i]);
-	i = -1;
-	while (info->cmd1[++i])
-		free(info->cmd1[i]);
+	i = 0;
+	while (info->path[i])
+		free(info->path[i++]);
+	free(info->path);
+	i = 0;
+	while (info->cmd1[i])
+		free(info->cmd1[i++]);
 	free(info->cmd1);
 	if (info->flags & relative)
 		free(info->path_cmd1);
-	i = -1;
-	while (info->cmd2[++i])
-		free(info->cmd2[i]);
+	i = 0;
+	while (info->cmd2[i])
+		free(info->cmd2[i++]);
 	free(info->cmd2);
 	if (info->flags & relative2)
 		free(info->path_cmd2);
-	free(info->path);
 }
