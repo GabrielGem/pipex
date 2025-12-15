d91e7e1d89c847be93ae1eb7f0bbffd86ba72105
gabrgarc <gabrgarc@student.42sp.org.br>
2025-12-14 21:38:55 -0300

    remove comments

--- pipex.h
+++ pipex.h
@@ -6,19 +6,19 @@
 /*   By: gabr <gabrgarc@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/08 11:34:32 by gabr              #+#    #+#             */
-/*   Updated: 2025/12/14 20:19:42 by gabrgarc         ###   ########.fr       */
+/*   Updated: 2025/12/14 21:38:20 by gabrgarc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PIPEX_H
 # define PIPEX_H
 
-# include <fcntl.h>   // open
-# include <unistd.h>  // close read write access dup dup2 execve fork pipe unlink
-# include <stdlib.h>  // malloc free exit
-# include <stdio.h>   // perror
-# include <string.h>  // strerror
-# include <sys/wait.h>// wait waitpid
+# include <fcntl.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <string.h>
+# include <sys/wait.h>
 # include "ft_printf.h"
 # include "libft.h"
 
