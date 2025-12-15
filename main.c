0f2fa9519e961238052573265e963fa456b09b0e
gabrgarc <gabrgarc@student.42sp.org.br>
2025-12-15 17:23:00 -0300

    rename call for function check_arguments

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: gabrgarc <gabrgarc@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/06 12:39:54 by gabrgarc          #+#    #+#             */
-/*   Updated: 2025/12/15 12:18:07 by gabrgarc         ###   ########.fr       */
+/*   Updated: 2025/12/15 17:16:11 by gabrgarc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int	main(int argc, char **argv, char **envp)
 {
 	t_info	info;
 
-	error_handle(argc, argv);
+	check_arguments_and_files(argc, argv);
 	info = get_info(argv, envp);
 	input_validations(&info);
 	pipex(&info);
